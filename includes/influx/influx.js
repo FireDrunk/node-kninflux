b904ace01f1bdf5d2eeca446af4cd0c7b4bff1d0
Thijs Cramer <thijs.cramer@gmail.com>
2017-02-05 13:40:07 +0100

    Rework influx

--- includes/influx/influx.js
+++ includes/influx/influx.js
@@ -14,23 +14,22 @@ exports.register_callback = function(name, func) {
 exports.connect = function(settings) {
   // Print settings
   if (DEBUG) console.log("[DEBUG] Settings: %j", settings);
+
   // Connect
   influx = new Influx.InfluxDB({
     host: settings.influx_db_host,
     port: settings.influx_db_port,
-    database: settings.influx_db_name || 'knx_db',
-    schema: [
-    {
-      measurement: settings.influx_db_table_name || 'knx_values',
-    		fields: {
-    			knx_address: Influx.FieldType.STRING,
-    			knx_value: Influx.FieldType.FLOAT
-    		},
-    		tags: [
-    			'name'
-    		]
-  	  }
-  	]
+    // database: settings.influx_db_name || 'knx_db',
+    // schema: [{
+    //   measurement: settings.influx_db_table_name || 'knx_values',
+  	// 	fields: {
+  	// 		knx_address: Influx.FieldType.STRING,
+  	// 		knx_value: Influx.FieldType.FLOAT
+  	// 	},
+  	// 	tags: [
+  	// 		'name'
+  	// 	]
+	  // }]
   });
 
   // Create influx db
