15e0de92bc62a9d6189d7e5975b3a4e8a4a56536
Thijs Cramer <thijs.cramer@gmail.com>
2017-02-05 14:02:13 +0100

    Rework error handling; influx writing data points

--- index.js
+++ index.js
@@ -13,8 +13,14 @@ var knx = require('./includes/knx/knx');
 var influx = require('./includes/influx/influx');
 
 // Settings
-var settings = JSON.parse(fs.readFileSync('settings.json', 'utf8'));
-if (DEBUG) console.log("[DEBUG] Settings: %j", settings);
+try {
+  var settings = JSON.parse(fs.readFileSync('settings.json', 'utf8'));
+  if (DEBUG) console.log("[DEBUG] Settings: %j", settings);
+}
+catch (err) {
+  if (DEBUG) console.error("[ERROR] Error parsing settings.json!");
+}
+
 
 // Environment
 var environment = JSON.parse(fs.readFileSync('environment.json', 'utf8'));
@@ -23,13 +29,18 @@ if (DEBUG) console.log("[DEBUG] Environment: %j", environment);
 // Events
 function on_knx_data_point_received(name, address, value) {
   if (DEBUG) console.log("[DEBUG] KNX Data point received, (%j, %j, %j)", name, address, value);
+  influx.write
 }
 
 // Callbacks
 function on_knx_connected() {
   if (DEBUG) console.log("[DEBUG] Connected to KNX");
+
+  // Push the environment to the KNX module
   knx.register_environment(environment);
   if (DEBUG) console.log("[DEBUG] Registered Environment");
+
+  // Start the read loop
   knx.start_reading(1000);
   if (DEBUG) console.log("[DEBUG] Started reading loop");
 }
